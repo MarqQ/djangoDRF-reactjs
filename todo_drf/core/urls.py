5a42ed0a3c437e0887ec28eb045f3eb5d826b42e
MarqQ <marcos.w.queiroz@gmail.com>
2020-08-31 11:49:10 -0300

    CRUD methods created

--- todo_drf/core/urls.py
+++ todo_drf/core/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.coreOverview, name="core-overview"),
     path('task-list/', views.tasklist, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+    path('task-create/', views.taskCreate, name="task-create"),
+    path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+    path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
 ]
