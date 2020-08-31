9d73f8b507e24f5f6635c01ef5217c127d8ace9a
MarqQ <marcos.w.queiroz@gmail.com>
2020-08-31 19:02:24 -0300

    add front end react project

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'reactdrf/build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'reactdrf/build/static')
+    os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 
 CORS_ORIGIN_WHITELIST = [
