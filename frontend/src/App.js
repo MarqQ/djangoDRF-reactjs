b15ed90f36692532f4d8b0136f8d5a7995264270
MarqQ <marcos.w.queiroz@gmail.com>
2020-09-02 13:05:10 -0300

    change react routes url to drf api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,7 +45,7 @@ class App extends React.Component {
   fetchTasks() {
     console.log('Fetching...');
 
-    fetch('https://react-django-drf.herokuapp.com/api/task-list/')
+    fetch('http://127.0.0.1:8000/api/task-list/')
       .then((response) => response.json())
       .then((data) =>
         this.setState({
@@ -74,10 +74,10 @@ class App extends React.Component {
 
     var csrftoken = this.getCookie('csrftoken');
 
-    var url = 'https://react-django-drf.herokuapp.com/api/task-create/';
+    var url = 'http://127.0.0.1:8000/api/task-create/';
 
     if (this.state.editing === true) {
-      url = 'https://react-django-drf.herokuapp.com/api/task-update/${this.state.activeItem.id}/';
+      url = 'http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}/';
       this.setState({
         editing: false,
       });
@@ -116,7 +116,7 @@ class App extends React.Component {
   deleteItem(task) {
     var csrftoken = this.getCookie('csrftoken');
 
-    fetch('https://react-django-drf.herokuapp.com/api/task-delete/${task.id}/', {
+    fetch('http://127.0.0.1:8000/api/task-delete/${task.id}/', {
       method: 'DELETE',
       headers: {
         'Content-type': 'application/json',
@@ -130,7 +130,7 @@ class App extends React.Component {
   strikeUnstrike(task) {
     task.completed = !task.completed;
     var csrftoken = this.getCookie('csrftoken');
-    var url = 'https://react-django-drf.herokuapp.com/api/task-update/${task.id}/';
+    var url = 'http://127.0.0.1:8000/api/task-update/${task.id}/';
 
     fetch(url, {
       method: 'POST',
